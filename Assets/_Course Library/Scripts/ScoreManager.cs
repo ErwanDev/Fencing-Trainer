d14b9982a6b1d151f2aaded46dd2ca04f9504e2d
Tianyi Yang <tianyi_yang@ucsb.edu>
2024-05-23 13:12:01 -0700

    initial attempt to add haptics

--- Assets/_Course Library/Scripts/ScoreManager.cs
+++ Assets/_Course Library/Scripts/ScoreManager.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public TextMeshProUGUI scoreText; 
     private int colorChangeCount = 0; 
+    private XRController controller = null;
 
     void Awake()
     {
+        controller = GetComponent<XRController>();
         // Ensure there is only one instance of the ScoreManager
         if (instance == null)
         {
@@ -23,6 +26,8 @@ public class ScoreManager : MonoBehaviour
     public void IncrementScore()
     {
         colorChangeCount++;
+        // Also send haptic feedback after score increase
+        if (controller != null) controller.SendHapticImpulse(.5f, .5f);
         UpdateScoreText();
     }
 
