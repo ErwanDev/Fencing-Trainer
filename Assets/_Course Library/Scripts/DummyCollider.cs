d45b61b65f41852420dce28cea171a2f1fd189d9
Xinyan Zhao <xinyan_zhao@ucsb.edu>
2024-05-29 21:05:12 -0700

    new Score System created

---
+++ Assets/_Course Library/Scripts/DummyCollider.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Fencing Foil Sword"))
+        {
+            ScoreSystem.scoreCount += 1;
+        }
+    }
+}
